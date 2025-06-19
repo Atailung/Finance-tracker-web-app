a976d45e9a308c767d9bc5999d50c1cb287be3c4
TaiLung <tailungart.business2023@gmail.com>
2025-06-19 18:22:25 +0545

    new version of code

--- NextJS-Frontend/src/components/landing/Hearder.tsx
+++ NextJS-Frontend/src/components/landing/Hearder.tsx
@@ -8,17 +8,15 @@ const Header = ({ isLoggedIn = false }) => {
   return (
     <nav className="bg-white dark:bg-gray-800 border-b border-gray-800 px-4 lg:px-6 py-2.5">
       <div className="max-w-screen-xl mx-auto flex items-center justify-between">
-        <Link href="https://flowbite.com" className="flex items-center space-x-3">
+        <Link href="/" className="flex items-center space-x-3">
           <Image
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="/logo.svg"
             alt="Fantrack Logo"
-            width={36}
-            height={36}
-            className="h-9"
+            width={120}
+            height={120}
+            className=""
           />
-          <span className="text-xl font-semibold text-gray-900 dark:text-white">
-            Fintrack
-          </span>
+          
         </Link>
         <Button
           color="primary"
